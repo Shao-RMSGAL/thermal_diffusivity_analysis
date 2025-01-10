579dabafab015bc67801f86375362e7552986ef7
Autonomous001 <bmejiad@tamu.edu>
2025-01-10 15:09:24 -0600

    For the future and normal people

---
+++ Cheatcodecommandjulia.sh
@@ -0,0 +1,10 @@
+julia --project=. -q -e "using ThermalDiffusivityGUI; ThermalDiffusivityGUI.julia_main();"
+
+To Actually run inside julia
+- first enter julia
+- second press ']'
+- command: activate .
+- if first time: 'instatiate'
+- exit package mode with Backspace
+- command: 'using ThermalDiffusivitGUI; ThermalDiffusivityGUI.julia_main();'
+- NOW IT SHOULD RUN
